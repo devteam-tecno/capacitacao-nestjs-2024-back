43174786bcc06d09d57718449e6eefc968066ded
matheusssilva991 <matheus.ssilva991@gmail.com>
2024-03-09 10:32:24 -0300

    feat: user dto

--- config/database.config.ts
+++ config/database.config.ts
@@ -2,7 +2,7 @@ import { DataSourceOptions } from 'typeorm';
 
 export const config: DataSourceOptions = {
   type: 'sqlite',
-  database: '../database/sqlite.db',
+  database: './database/sqlite.db',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
 };
--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,12 +1,28 @@
+import { IsEmail, IsString, IsStrongPassword } from 'class-validator';
+
 export class CreateUserDto {
   // Validar que nome é string
+  @IsString({ message: 'Nome deve ser uma string.' })
   name: string;
 
   // Validar que email é string
   // Validar que email é um email
+  @IsString({ message: 'Email deve ser uma string.' })
+  @IsEmail({}, { message: 'Email deve ser um email.' })
   email: string;
 
   // Validar que password é string
   // Validar que password tem no mínimo 6 caracteres
+  @IsString({ message: 'Senha deve ser uma string.' })
+  @IsStrongPassword(
+    {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    },
+    { message: 'Senha deve ter no mínimo 6 caracteres.' },
+  )
   password: string;
 }
