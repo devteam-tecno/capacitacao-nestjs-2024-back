eae12a0212468fdef360f1a796b6d5ce97c6d185
matheusssilva991 <matheus.ssilva991@gmail.com>
2024-03-23 10:23:44 -0300

    feat: recursos adicionais

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: false }));
 
+  app.enableCors({
+    origin: 'http://localhost:3001', // Quais endereço de ip podem acessar a API
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Quais métodos podem ser utilizados
+    allowedHeaders: 'Content-Type, Accept', // Quais headers podem ser enviados
+  });
+
+  //app.enableCors(); // Permite que qualquer endereço de ip acesse a API
   const port = app.get(ConfigService).get('PORT') || 3000;
   await app.listen(port);
 }
 bootstrap();
+// http://localhost:3000 -> Back-end
+// http://localhost:3001 -> Front-end
